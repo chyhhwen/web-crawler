5cfbb7263f1ad0cee058fd979839fef1e537c67c
chyhhwen <chyhhwen@gmail.com>
2023-03-13 19:31:58 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# web-crawler
+# 網路爬蟲
+## 套件
+`pip install selenium` 載入selenium套件\
+`https://chromedriver.chromium.org/downloads` 下載driver套件
+## 語法
+`webdriver.Chrome`開啟chrome\
+`driver.get` 取得網址\
+`find_elements` 尋找網頁內容位置
+
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+def login():
+    option = webdriver.ChromeOptions()
+    option.add_experimental_option("detach", True)
+    path = "C:/Users/User/Desktop/web-crawler/lib/chromedriver.exe"
+    driver = webdriver.Chrome(path, options = option)
+    print("今日股價")
+    #台積電
+    url = "https://tw.search.yahoo.com/search?p=2330&fr=finance&fr2=p%3Afinvsrp%2Cm%3Asb"
+    driver.get(url)
+    elem = "fin_quotePrice"
+    title = driver.find_elements(By.CLASS_NAME, elem)
+    for i in title:
+        print("台積電:" + i.text)
+    #聯發科
+    url = "https://tw.search.yahoo.com/search;_ylt=Awrt4BvBBw9keQ0U425r1gt.;_ylc=X1MDMjExNDcwNTAwMwRfcgMyBGZyA2ZpbmFuY2UEZnIyA3NiLXRvcARncHJpZANkdjJ5dExDZ1EzS0lRME05YXRscGxBBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4DdHcuc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAzAEcXN0cmwDNARxdWVyeQMyNDU0BHRfc3RtcAMxNjc4NzA2Njc5?p=2454&fr2=sb-top&fr=finance"
+    driver.get(url)
+    elem = "fin_quotePrice"
+    title = driver.find_elements(By.CLASS_NAME, elem)
+    for i in title:
+        print("聯發科:" + i.text)
+    # 鴻海
+    url = "https://tw.search.yahoo.com/search;_ylt=AwrtgxItCA9kvy4UTDRr1gt.;_ylc=X1MDMjExNDcwNTAwMwRfcgMyBGZyA2ZpbmFuY2UEZnIyA3NiLXRvcARncHJpZANaU0g1anBUNVRTYTFUV0NTNTYwZk9BBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4DdHcuc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAzAEcXN0cmwDNARxdWVyeQMyMzE3BHRfc3RtcAMxNjc4NzA2NzM0?p=2317&fr2=sb-top&fr=finance"
+    driver.get(url)
+    elem = "fin_quotePrice"
+    title = driver.find_elements(By.CLASS_NAME, elem)
+    for i in title:
+        print("鴻海:" + i.text)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    driver.quit()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    login()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
